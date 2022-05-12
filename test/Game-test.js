7115894a53a3de49f31f4774ea56bad1017e56c3
Blue Jessen <canealisblue@gmail.com>
2022-05-12 13:34:55 -0700

    Correct round class and corresponding tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,10 +1,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const data = require('../src/data');
-const prototypeQuestions = data.prototypeData;
-const util = require('../src/util');
-
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
