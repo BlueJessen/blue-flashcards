667e89e8dade2c435b66472de7b6f62856d55a1d
Blue Jessen <canealisblue@gmail.com>
2022-05-12 13:42:50 -0700

    Add arrow functions to tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,55 +4,55 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
-describe('Turn', function() {
-  it('should be a function', function() {
+describe('Turn', () => {
+  it('should be a function', () => {
     const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it('should store user answer', function() {
+  it('should store user answer', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('Example', card);
 
     expect(turn.userGuess).to.equal('Example');
   });
 
-  it('should store the current card', function() {
+  it('should store the current card', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('Example', card);
 
     expect(turn.card).to.deep.equal(card);
   });
 
-  it('should have a returnGuess function', function() {
+  it('should have a returnGuess function', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('Example', card);
 
     expect(turn.returnGuess()).to.equal('Example');
   });
 
-  it('should have a returnCard function', function() {
+  it('should have a returnCard function', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('Example', card);
 
     expect(turn.returnCard()).to.deep.equal(card);
   });
 
-  it('should have a evaluateGuess function', function() {
+  it('should have a evaluateGuess function', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('Example', card);
 
     expect(turn.evaluateGuess()).to.equal(true);
   });
 
-  it('evaluateGuess should test if user guess is correct', function() {
+  it('evaluateGuess should test if user guess is correct', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('example', card);
 
     expect(turn.evaluateGuess()).to.equal(false);
   });
 
-  it('should have a giveFeedback function', function() {
+  it('should have a giveFeedback function', () => {
     const card = new Card(1,'Example?',['example','Example'], 'Example');
     const turn = new Turn('Example', card);
     const nextCard = new Card(2,'Test?',['Test','test'], 'test');
